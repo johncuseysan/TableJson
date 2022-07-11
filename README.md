1c70405857cc70d3f1ae87516731aac384d7c405
John Cusey Sandbox <johncuseysan@gmail.com>
2022-07-11 00:54:26 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# TableJson
+# Table Json
+
+# Resource    
+[How to access variables from another file using JavaScript](https://www.geeksforgeeks.org/how-to-access-variables-from-another-file-using-javascript/)
---
+++ database.js
@@ -0,0 +1,19 @@
+var Student = [
+    {
+        name : "Pete Johnson",
+        age : 18,
+        dept : "CSE",
+        score : 90
+    },
+    {
+        name : "Tom Smith",
+        age : 23,
+        dept : "HIS",
+        score : 87
+    }
+];
+
+    
+    
+
+    
