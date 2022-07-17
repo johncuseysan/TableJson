880633e8621469d8444d1c272909b3feb9696b5b
John Cusey Sandbox <johncuseysan@gmail.com>
2022-07-17 16:19:20 -0400

    Finishing Body Class

--- public/dev/main_body.js
+++ public/dev/main_body.js
@@ -1,8 +1,26 @@
 var db = require("../js/database");
-var Header = require("../js/body");
+
+var Header = require("../js/header");
+var Body = require("../js/body");
 
 console.log("PROGRAM STARTED");
 
 /***  Get The Database Array Objects ***/
 console.log(" Student list from the Database \n");
 console.log(db.students);
+
+/***  Get The Keys from the Database Array Objects ***/
+var header = new Header(db.students);
+
+header.buildHeaderList();
+
+console.log(" Header list from the Database \n");
+console.log(header.getTableHeader());
+
+/*** ***/
+var body = new Body(header.getTableHeader(), db.students);
+
+body.buildBodyList();
+
+console.log(" Data list from the Database \n");
+console.log(body.getTable());
--- public/js/body.js
+++ public/js/body.js
@@ -1,8 +1,62 @@
 class Body{
 
+    constructor(header, students) {
+        this.table_header = header;
+        this.table_body = new Array();
+    
+        this.students = students;
+        this.table = "";
+    }
+
+    setTableHeader(val){this.table_header = val;}
+    getTableHeader() {return this.table_header;}
+
+    setTableBody(val){this.table_body = val;}
+    getTableBody() {return this.table_body;}
+
+    setTable(val){this.table = val;}
+    getTable() {return this.table;}
+
+    setStudents(val){this.students = val;}
+    getStudents() {return this.students;}
 
 
+    buildBodyList(){
 
+        console.log("*** buildBodyList \n");
+
+        if( ! Array.isArray(this.table_header) ){
+
+            //Converting Set to Array
+            this.table_header = Array.from(this.table_header);
+        }
+
+
+        for(var m=0; m <this.students.length; m++ ){
+
+            //This is the JSON Data from the database.js
+            var element = this.students[m];
+
+            this.table = this.table + "<tr>";
+
+            for(var i=0; i <this.table_header.length; i++ ){
+
+                var table_data = element[this.table_header[i]];
+
+                if (table_data === undefined || table_data === null){
+                    this.table = this.table + "<td></td>";
+                }else{
+                    this.table = this.table + "<td>" + table_data + "</td>";
+                }
+
+            }
+
+            this.table = this.table + "</tr> \n";
+
+            
+        }
+
+    }
 
 }
 
